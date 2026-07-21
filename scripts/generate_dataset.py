e2b0a56ecf0b9fe5f63b6f81e68780d29151d4e9
Piero-Lujan-Pedreschi <piero.lujanpedreschi10@gmail.com>
2026-07-20 17:43:01 -0700

    another fix

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
     reference_spectrum = np.mean(spectra, axis=0)
 
     # Ex.) REPO_ROOT/data/co_terpy_L3L2_reference_spectrum.npy
-    config_spec_type = args.config[:-10]
+    config_spec_type = config[:-10]
     reference_path = REPO_ROOT / 'data' / f'{config_spec_type}_reference_spectrum.npy'
 
     np.save(str(reference_path), reference_spectrum)
