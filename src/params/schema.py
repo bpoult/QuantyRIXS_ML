3ada5835230f36041ad7973ac2d0b7c6bdec610d
Piero-Lujan-Pedreschi <piero.lujanpedreschi10@gmail.com>
2026-07-27 16:20:08 -0700

    changing x-axis shift in training data

--- src/params/schema.py
+++ src/params/schema.py
@@ -35,7 +35,7 @@ class CrystalFieldParams:
     U_2p_3d_f: float = 6.0
     scalef2_2p3d: float = 0.2
     scale_2pSOC: float = 0.8
-    E_2p: float = 756.0
+    E_2p: float = 754.7
 
     # --- initial state ---
    
